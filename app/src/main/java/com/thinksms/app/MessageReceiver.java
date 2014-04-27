a9d6c358d04824c240f1667b32af8d35c0ac5740
Dan Treiman <dan.treiman@gmail.com>
2014-04-26 17:08:50 -0700

    Added support for GCM/push notifications through Parse.com.

---
+++ app/src/main/java/com/thinksms/app/MessageReceiver.java
@@ -0,0 +1,41 @@
+package com.thinksms.app;
+
+/**
+ * Created by dtreiman on 4/26/14.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+/**
+ * Created by dtreiman on 4/26/14.
+ */
+public class MessageReceiver extends BroadcastReceiver {
+    private static final String TAG = "MessageReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            String action = intent.getAction();
+            String channel = intent.getExtras().getString("com.parse.Channel");
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+
+            JSONObject dataObject = json.getJSONObject("data");
+
+            String text = dataObject.getString("text");
+
+            NotifyIntentService.presentNotificationForMessage(context, text);
+        }
+        catch (JSONException e) {
+            Log.d(TAG, "JSONException: " + e.getMessage());
+        }
+    }
+
+}
